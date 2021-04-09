007aba41863dc000c46097f388be75678a1c84ec
Amanda Sousa Bezerra <amanda.sousabez@gmail.com>
2021-04-09 14:46:50 -0300

    buscando por primary key

--- instagram.js
+++ instagram.js
@@ -11,3 +11,16 @@ Post.findAll().then((resultado) => {
 Comentario.findAll().then((resultado) => {
     console.table(resultado.map(user => user.toJSON()));
 });
+
+// buscado pela chave primaria:
+Usuario.findByPk(1).then((resultado) => {
+    console.table(resultado.toJSON()); 
+});
+
+Comentario.findByPk(2).then((resultado) => {
+    console.table(resultado.toJSON()); 
+});
+
+Post.findByPk(1).then((resultado) => {
+    console.table(resultado.toJSON()); 
+});
