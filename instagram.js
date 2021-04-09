2b8a6a86bed96b2810ec481bf680e7c54552961e
Amanda Sousa Bezerra <amanda.sousabez@gmail.com>
2021-04-09 14:36:30 -0300

    tabela de comentarios e posts

--- instagram.js
+++ instagram.js
@@ -1,6 +1,13 @@
-const { Usuario, sequelize } = require('./models');
+const { Usuario, Post, Comentario, sequelize } = require('./models');
 
-Usuario.findAll()
-.then((resultado) => {
-    console.log(resultado);
-})
+Usuario.findAll().then((resultado) => {
+    console.table(resultado.map(user => user.toJSON()));
+});
+
+Post.findAll().then((resultado) => {
+    console.table(resultado.map(user => user.toJSON()));
+});
+
+Comentario.findAll().then((resultado) => {
+    console.table(resultado.map(user => user.toJSON()));
+});
--- models/Comentario.js
+++ models/Comentario.js
@@ -1,16 +1,15 @@
 module.exports = (sequelize, DataTypes) => {
+
+    //define (nomeModel, colunas, config)
     const Comentario = sequelize.define(
         'Comentario', {
-            id: DataTypes.STRING,
-            texto: DataTypes.STRING,
-            usuarios_id: DataTypes.INTEGER,
-            posts_id: DataTypes.INTEGER  
-        },
-        {
-            tableName: "comentarios",
+           texto: DataTypes.STRING,
+           usuarios_id: DataTypes.INTEGER,
+           posts_id: DataTypes.INTEGER
+         }, {
+            tableName: 'comentarios',
             timestamps: false
         }
     );
-
-    return Comentario;
+        return Comentario;
 }
