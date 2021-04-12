48b800bfb57eaad07c2f7326e078f7936ac81ecb
Amanda Sousa Bezerra <amanda.sousabez@gmail.com>
2021-04-12 10:58:49 -0300

    add findAll com limite: 2 por vez

--- instagram.js
+++ instagram.js
@@ -1,26 +1,52 @@
 const { Usuario, Post, Comentario, sequelize } = require('./models');
+const { Op } = require("sequelize");
 
-Usuario.findAll().then((resultado) => {
-    console.table(resultado.map(user => user.toJSON()));
-});
+//users com letra a no nome
+// Usuario.findAll({
+//     where: {
+//         nome: {[Op.like]: '%a%'},
+//     },
+// }).then((resultado) => {
+//     console.table(resultado.map(user => user.toJSON()));
+// });
 
-Post.findAll().then((resultado) => {
-    console.table(resultado.map(user => user.toJSON()));
-});
+//users sem letra a no nome
+// Usuario.findAll({
+//     where: {
+//         nome: {[Op.notLike]: '%a%'},
+//     },
+// }).then((resultado) => {
+//     console.table(resultado.map(user => user.toJSON()));
+// });
 
-Comentario.findAll().then((resultado) => {
-    console.table(resultado.map(user => user.toJSON()));
-});
+// Post.findAll().then((resultado) => {
+//     console.table(resultado.map(user => user.toJSON()));
+// });
 
-// buscado pela chave primaria:
-Usuario.findByPk(1).then((resultado) => {
-    console.table(resultado.toJSON()); 
-});
+var i = 0;
+do{
+    Comentario.findAll({
+        order: [
+            ['id']
+        ],
+        limit: 2,
+        offset: i,
+    }).then((resultado) => {
+        console.table(resultado.map(user => user.toJSON()));
+    })
+    i += 2;
+}while(i<6);
 
-Comentario.findByPk(2).then((resultado) => {
-    console.table(resultado.toJSON()); 
-});
 
-Post.findByPk(1).then((resultado) => {
-    console.table(resultado.toJSON()); 
-});
+// // buscado pela chave primaria:
+// Usuario.findByPk(1).then((resultado) => {
+//     console.table(resultado.toJSON()); 
+// });
+
+// Comentario.findByPk(2).then((resultado) => {
+//     console.table(resultado.toJSON()); 
+// });
+
+// Post.findByPk(1).then((resultado) => {
+//     console.table(resultado.toJSON()); 
+// });
